50edb2d30cf2e0c644197e7dcf9de359dd8b38b1
Dudxxx <130068099+Dudxxx@users.noreply.github.com>
2024-08-21 11:00:24 -0300

    .

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export function App() {
+  return (
+    <>
+      <h1>Boizão</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse debitis perferendis minus temporibus facilis? Excepturi dignissimos minima impedit alias, soluta ut laboriosam vero id voluptatibus ducimus nesciunt, provident corporis doloribus?</p>
+    </>
+  ) 
+}
