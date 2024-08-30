b2e83a89ae0e2c931795a475056c7f7f4198d0d0
Dudxxx <schimidteduardo941@gmail.com>
2024-08-30 09:22:49 -0300

    .

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,9 +2,16 @@ import styles from "./Main.module.css"
 
 export function Main() {
     return (
-        <>
-            <h3>
-            </h3>                    
-        </>
+        <div className={styles.Main}>
+            <form action="">
+                <input type="text" id="input" placeholder="  React"/>
+                <button type="button">+</button>
+            </form>
+            <ol className={styles.List}>
+                <li>Item 1 <button>X</button></li>
+                <li>Item 2 <button>X</button></li>
+                <li>Item 3 <button>X</button></li>
+            </ol>
+        </div>
     )
 }
